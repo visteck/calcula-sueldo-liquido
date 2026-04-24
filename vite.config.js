2d2b7ffea7e16f006ad758475ab3a4836a2c73c5
Victor Cabrera <vcabrera@contaline.cl>
2026-04-24 11:19:55 -0400

    fix: corregir ruta base de /apis/calculadora-sueldo-liquido/ a /apps/calcula-sueldo-liquido/

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/apis/calculadora-sueldo-liquido/'
+  base: '/apps/calcula-sueldo-liquido/'
 })
